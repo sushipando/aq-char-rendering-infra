46cf7df25f3d1e70cb620074659d066e17725e76
sushipando <sushipando@proton.me>
2026-08-28 07:12:38 -0700

    Initial commit

---
+++ test/aqw-char-rendering-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AqwCharRenderingInfra from '../lib/aqw-char-rendering-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aqw-char-rendering-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AqwCharRenderingInfra.AqwCharRenderingInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
